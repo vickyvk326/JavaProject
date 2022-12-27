27fa03b5c5d1888148e7bb7c5551c7628c92eaf8
vickyvk326 <vigneshravi326@gmail.com>
2022-12-27 10:52:09 +0530

    commit message

---
+++ Rectangle.java
@@ -0,0 +1,13 @@
+package test;
+
+public class Rectangle {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		float l =10f, b = 12f;
+		double area = l*b, perimeter = 2 * (l+b);
+		System.out.println("Area = "+area+"\nPerimeter = "+perimeter);
+
+	}
+
+}
